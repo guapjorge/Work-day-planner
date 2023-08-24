5a7b33744e8bf66127eaec4bf858a1d614769a94
guapjorge <alcalajorgea@gmail.com>
2023-08-24 13:24:05 -0700

    finished producted

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+function displayDateTime() {
+    var currentDayOg = $('#currentDay');
+    var currentTimeOg = $('#currentTime');
+    currentDayOg.text(dayjs().format('dddd MMMM D, YYYY'));
+    currentTimeOg.text(dayjs().format('h[:]mm a'));
+}
+
+$(function () {
+    $('.saveBtn').on('click', function () {
+        const description = $(this).siblings('.description').val();
+        const time = $(this).parent().attr('id');
+
+        localStorage.setItem(time, description);
+    });
+    displayDateTime();
+    for (let i = 9; i < 18; i++) {
+        $(`#hour-${i + 1} .description`).val(localStorage.getItem(`hour-${i + 1}`));
+    };
+
+    $('.time-block').each(function () {
+        const currentTime = dayjs().hour();
+        const calHour = parseInt($(this).attr('id').split('-')[1]);
+        console.log(currentTime, calHour)
+        if (currentTime > calHour) {
+            $(this).addClass('past');
+        } else if (currentTime === calHour) {
+            $(this).addClass('present');
+        } else {
+            $(this).addClass('future')
+        }
+        console.log(calHour)
+    })
+});
